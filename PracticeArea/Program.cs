7ef8e695f8d96d0acbe137c36bf1fd20588c68bd
Mustafa Güler <mail.mustafaguler@gmail.com>
2025-10-16 22:30:41 +0300

    second push

--- PracticeArea/Program.cs
+++ PracticeArea/Program.cs
@@ -8,7 +8,7 @@ namespace PracticeArea
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!, test");
         }
     }
 }
